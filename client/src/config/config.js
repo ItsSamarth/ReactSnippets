875f9647a71f50768cf72a6daa5db4d2bb1c9852
samarth <samarth@tricog.com>
2019-12-26 13:06:10 +0530

    Checkpoint chunking

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,16 @@
 let apiConfig = require("./api.json");
 let base_url = apiConfig.api;
 const CONFIG = {
-  base_url: base_url
+  base_url: base_url,
+  s3: {
+    fields: {
+      AWSAccessKeyId: "",
+      acl: "public-read",
+      key: ""
+    },
+    fileUrl: "",
+    uploadUrl: ""
+  }
 };
 
 export default CONFIG;
